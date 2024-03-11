7b4f7a1a9b3bbe97591d0e133aa2af1e321aa9e9
LenioJunior <lenio.prado@ifsuldeminas.edu.br>
2024-03-11 10:14:54 -0300

    Complementação - Aula 2

--- Complementacao/js/handleForm.js
+++ Complementacao/js/handleForm.js
@@ -3,14 +3,22 @@ var header = $("#header");
 
 $(document).ready(function() {
     console.log("Página Carregada!");
-  } );
+    $("#includedContent").load('pages/Home1.html');
+});
 
-$(document).on("click", header, function(e){
+function setAsActive(e){
     console.log("Elemento Clicado: ", e.target.text);
     var active = $('.nav-link.active');
     console.log("Elemento Atualmente Ativo: ", active);
     $(active[0]).removeClass("active");
     $(e.target).addClass("active");
+}
+
+$(document).on("click", header, function(e){
+    setAsActive(e);
+    var pageName = e.target.text;
+    $("#includedContent").load('pages/' + pageName + '1.html');
+
 });
 
 //console.log(doc);
