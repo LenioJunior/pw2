2c4d7f36cad12f5539ae7520860f5fd4975f442f
LenioJunior <lenio.prado@ifsuldeminas.edu.br>
2024-03-20 17:27:27 -0300

    Início dev SCE na aula

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
         <ul class="dropdown-menu">
           <li><a class="dropdown-item" href="Aulas/index.php">Aulas</a></li>
           <li><a class="dropdown-item" href="sce/index.php">Sistema de Controle de Estoque</a></li>
+          <li><a class="dropdown-item" href="sce_aula/index.php">SCE Aula</a></li>
           <li><a class="dropdown-item" href="Complementacao/index.php">Complementação</a></li>
         </ul>
       </div>
